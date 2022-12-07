778fff979f0269c765bd58ee0a5b17bf70b1b812
jadeven <deveneyjoseph@gmail.com>
2022-12-06 15:07:22 -1000

    activity 1 7.2

--- components/views/Home.js
+++ components/views/Home.js
@@ -1,8 +1,12 @@
 import html from "html-literal";
 
-export default () => html`
+export default state => html`
   <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html">"Call to Action" "Button"</a>
+    <a href="index.html"
+      >"Call to Action" "Button"
+      <p>${state.weather.city}</p>
+      <p>${state.weather.temp} degrees</p></a
+    >
   </section>
 `;
